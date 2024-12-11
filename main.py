02c134ea630d34cc2872a9fe10f6691d3b8525bd
Ramsés Riofrio <134115228+RamCupido@users.noreply.github.com>
2024-12-10 20:53:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Se crear la aplicación FastAPI
 app = FastAPI()
 
+#Clase para crear un usuario con varios parametros
+class User(BaseModel):
+    id: str
+    email: str
+    age: int
+    is_admin: bool
+
+#Metodo sin parametros
 @app.get("/")
-def index():
-    #Retorna el mensaje que nostros queramos
+def root():
+    #Retorna el mensaje que nostros queramos en formato JSON
     return { "mensaje" : "Hola Mundo"}
 
-if __name__ == "__main__":
-    #Inicia el servidor de FastAPI con Uvicorn
-    uvicorn.run("main:app", port=8000, reload=True)
+#Metodo que tiene parametros como el id y el cat
+@app.get("/users/{cat}")
+def get_users (id: int, cat):
+    #Retorna el id de tipo int y el cat (JSON)
+    return {"id": id, "cat": cat}
+
+#Metodo que ingresa 1 usuario
+@app.post("/users")
+def save_users(user: User):
+    return user
